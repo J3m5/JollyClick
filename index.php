70e3665fc6591ceaceb4e07d5302d5b5c18731ac
J3m5 <anojems@gmail.com>
2017-06-29 16:48:59 +0200

    eyge

--- index.php
+++ index.php
@@ -13,19 +13,19 @@ include("includes/header.php");
             <div class="slider-short-header">
                 <p class="suggestion">Suggestion de profils</p>
             </div>
-<?php            
+<?php
 include("includes/resultat.php");
 ?>
-             
+
         </div>
     </div>
-
+test
 
 
 <script src="static/external/jquery/dist/jquery.min.js"></script>
 <script src="static/external/bootstrap/dist/js/bootstrap.min.js"></script>
 
-  
+
 
 
 
